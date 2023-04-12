8e7d3f930e12b3768673f079534cc229a4bdb51f
sriee19 <srisanjanaarunkumar@gmail.com>
2023-04-12 09:53:58 +0530

    React files installed

---
+++ travel/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Amigos</h1>
+      <h2>Start editing to see some magic happen! </h2>
+    </div>
+  );
+}
+
+export default App;
