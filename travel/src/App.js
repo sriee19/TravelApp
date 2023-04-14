6837afd970ba065193bd451934c5b806ebaef69f
sriee19 <srisanjanaarunkumar@gmail.com>
2023-04-14 21:10:01 +0530

    react started

--- travel/src/App.js
+++ travel/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello Amigos</h1>
-      <h2>Start editing to see some magic happen! </h2>
+      <h2>Welcome to amigos world </h2>
     </div>
   );
 }
