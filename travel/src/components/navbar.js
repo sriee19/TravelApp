c13c1acffa44492723a03f9bb37712eb8921c90e
sriee19 <srisanjanaarunkumar@gmail.com>
2023-04-17 01:30:14 +0530

    navbar on click and css alterations

--- travel/src/components/navbar.js
+++ travel/src/components/navbar.js
@@ -1,12 +1,22 @@
 import { component } from "vue/types/umd";
 import "./navbar.css";
 import {menuitems} from './menuitems';
+import { link } from "reac-router-dom";
+
 
 class Navbar extends component{
+    state = {clicked: false};
+    handleClick = () => {
+        this.setState( { clicked : !this.state.clicked })
+    }
     render(){
         return(
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">Tours</h1>
+                <div className="menu-icons" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                    {/* <i className="fas fa-times"></i> */}
+                </div>
                 <ul className="nav-menu">
                     {menuitems.map((item, index) =>{
                         return(
